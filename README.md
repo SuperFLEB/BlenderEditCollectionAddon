422465a21c389ae3168289beebeffbbe63e333cf
FLEB <voterudy@voterudy.org>
2022-07-25 22:13:24 -0400

    Change directory structure Add a preference to suppress the popup upon entering a temporary Scene Add a blurb that mentions the preference Fix: A new context menu item is made on each reload of the addon Update README to be that little bit better Other minor tweaks

--- README.md
+++ README.md
@@ -1,33 +1,26 @@
 # Blender "Edit Instanced Collection" Addon
-An add-on for Blender that allows quickly editing Collection Instance source collections
-
-*:construction: If you're reading this, it's literally the first revision that I just threw up to have somewhere to put this. It's not
-terribly complicated, so it should all work, but the docs and polish aren't up to snuff.*
+An add-on for Blender that allows quickly editing Collection Instance source collections.
 
 This came about as the result of making Asset Library files that used Collection Instances to house multiple-Object assets.
 Upon importing them into another file, they would be seemingly uneditable, as Blender would link the Collection Instances to
 the current Scene, but not the Collection itself.
 
-After some searching around, I found that the collections were attached to the file (accessible via the Blender File view in
-the outliner), but that was hidden away and difficult to work with (attaching them back to the Scene using "Link to Scene"
-worked, but then you have an extra copy hanging around your scene).
-
-Since it was a relatively reprocducible process to find and link the Collections, I created this addon that allows quickly
-dropping in to edit the underlying Collection of a Collection Instance.
+Since it was a relatively reproducible process to find and link the Collections, I created this addon that allows quickly
+dropping in to edit the underlying Collection of a Collection Instance, and then dropping out.
 
 ## To Install
 
-Put the "edit_instanced_collection" directory into your Blender/3.x/scripts/addons directory and, y'know, light 'er up in
-your prefs.
-
-(I'll get it packaged up properly into a release sometime soon here, and give proper installation instructions. It's really
-late, I'm one foot in the bed, and I'm in no condition to do things prettily right now. Watch this space.)
+It's an ordinary Blender addon. You can either install the ZIP release and install it, or clone/copy this into your
+Blender addons directory.
 
 ## To Use
 
-Select a Collection Instance. Selecting multiple Collection Instances is not supported, and the active item must be a Collection
-Instance to see the menu. Either from the Object menu or the spacebar/W menu, select "Edit Instanced Collection". You will be
-dropped into a newly-created Scene containing nothing but the Collection. You can edit and add or remove items, which will be
-reflected in all instances of the Collection. Once you are done editing (ensure that all new objects are within the Collection)
-delete the temporary Scene (named "temp(name-of-collection)"). You can drill down to multiple levels, though you will have to be
-sure to clean up any temporary Scenes made by the addon.
+1. Select a Collection Instance. (Selecting multiple Collection Instances is not supported.)
+2. Either from the Object menu or the spacebar/W context menu, select "Edit Instanced Collection".
+3. You will be dropped into a newly-created Scene containing nothing but the Collection. Edit the collection, and your
+   edits will be reflected in all instances of the Collection.
+4. Once you are done editing (ensure that all new objects are within the Collection) delete the temporary Scene (named
+   "temp(name-of-collection)").
+
+You can drill down to multiple levels, though you will have to be sure to clean up any temporary Scenes
+made by the addon.
---
+++ __init__.py
@@ -0,0 +1,123 @@
+import bpy
+
+bl_info = {
+    "name": "Edit Instanced Collection",
+    "description": "Edit a Collection Instance's source Collection (even if it is not in the Scene).",
+    "author": "FLEB",
+    "version": (0, 1, 0),
+    "blender": (3, 1, 0),
+    "location": "Object > Edit Instanced Collection",
+    "doc_url": "https://github.com/SuperFLEB/BlenderEditCollectionAddon",
+    "tracker_url": "https://github.com/SuperFLEB/BlenderEditCollectionAddon/issues",
+    "support": "COMMUNITY",
+    "category": "Object",
+}
+
+addon_keymaps = []
+package_name = __package__
+seen_popup = False
+
+
+class EditCollection(bpy.types.Operator):
+    """Edit the Collection referenced by this Collection Instance in a new Scene"""
+    bl_idname = "object.edit_collection"
+    bl_label = "Edit Instanced Collection"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        prefs = context.preferences.addons[package_name].preferences
+        coll = bpy.context.active_object.instance_collection
+
+        if not coll:
+            print("Active item is not a collection instance")
+            self.report({"WARNING"}, "Active item is not a collection instance")
+            return {"CANCELLED"}
+
+        scene_name = f"temp:{coll.name}"
+        bpy.ops.scene.new(type="EMPTY")
+        new_scene = bpy.context.scene
+        new_scene.name = scene_name
+        bpy.context.window.scene = new_scene
+        new_scene.collection.children.link(coll)
+        # Select the collection
+        bpy.context.view_layer.active_layer_collection = bpy.context.view_layer.layer_collection.children[coll.name]
+
+        def message(self_2, _):
+            global seen_popup
+
+            self_2.layout.label(text=f"When you're done, simply delete the scene using the X ")
+            self_2.layout.label(
+                text=f"Be sure to keep all changes you wish to apply within the \"{coll.name}\" collection.")
+            # Only show the message about it being able to be turned off the first time
+            if not seen_popup:
+                seen_popup = True
+                self_2.layout.separator()
+                self_2.layout.label(
+                    text=f"(This message can be turned off in the {bl_info['name']} addon preferences panel.)",
+                    icon="INFO"
+                )
+
+        if not prefs.hide_scene_popup:
+            bpy.context.window_manager.popup_menu(message,
+                                                  title=f"Temporary Scene \"{scene_name}\" Created",
+                                                  icon='COLLECTION_NEW')
+
+        return {"FINISHED"}
+
+
+class EditInstancedCollectionPreferences(bpy.types.AddonPreferences):
+    bl_idname = package_name
+    hide_scene_popup: bpy.props.BoolProperty(
+        name="Don't show instructional popup message",
+        description="Do not show the informational pop-up when editing a Collection",
+        default=False
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, 'hide_scene_popup')
+
+
+def menu_function(self, context):
+    if bpy.context.active_object.instance_collection:
+        self.layout.operator(EditCollection.bl_idname)
+
+
+def register():
+    global seen_popup
+    seen_popup = False
+
+    # Register classes
+    bpy.utils.register_class(EditCollection)
+    bpy.utils.register_class(EditInstancedCollectionPreferences)
+
+    # Add menu items
+    bpy.types.VIEW3D_MT_object.append(menu_function)
+    bpy.types.VIEW3D_MT_object_context_menu.append(menu_function)
+
+    # Add keymaps
+    wm = bpy.context.window_manager
+    if wm.keyconfigs.addon:
+        km = wm.keyconfigs.addon.keymaps.new(name='Object Mode', space_type='EMPTY')
+        kmi = km.keymap_items.new(EditCollection.bl_idname, 'C', 'PRESS', ctrl=True, alt=True)
+        # kmi.properties.total = 4
+        addon_keymaps.append((km, kmi))
+
+
+def unregister():
+    # Unregister classes
+    bpy.utils.unregister_class(EditCollection)
+    bpy.utils.unregister_class(EditInstancedCollectionPreferences)
+
+    # Remove menu items
+    bpy.types.VIEW3D_MT_object.remove(menu_function)
+    bpy.types.VIEW3D_MT_object_context_menu.remove(menu_function)
+
+    # Clear keymaps
+    for km, kmi in addon_keymaps:
+        km.keymap_items.remove(kmi)
+    addon_keymaps.clear()
+
+
+if __name__ == '__main__':
+    register()
